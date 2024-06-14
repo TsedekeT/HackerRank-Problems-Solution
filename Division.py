0032f183825a9c1e0867eb6576d7269c148e00d5
Tsedeke <164669795+TsedekeT@users.noreply.github.com>
2024-06-14 21:11:42 +0300

    Create Division.py

---
+++ Division.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    result_integer_division = a//b
+    result_float_division = a/b
+    
+print(result_integer_division) 
+print(result_float_division)   
+    
+     
+     
